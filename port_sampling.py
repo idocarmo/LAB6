72d541ad50fdfb5947bd204b4a7bfb43164d4005
Ivens <ivens.costa@usp.br>
2022-12-11 22:55:57 -0300

    Port sampling fix

--- port_sampling.py
+++ port_sampling.py
@@ -1,20 +1,33 @@
 import pigpio
 import numpy as np
+
+import sys
 import time
+import logging
+
+logging.basicConfig(
+    filename = 'file.log',
+    level = logging.INFO,
+    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+    )
+
 
 GPIO = 27
 
 N_BITS = 16*100
 N_TIME_CAPTURES = N_BITS + 2 + 1
 MEASURE = True
+
 capture_array = np.array((N_TIME_CAPTURES, 2))
 i=0
 
 def array_write(gpio, level, tick):
     capture_array[i] = tick, level
     i += 1
+    if i == (N_TIME_CAPTURES - 1):
+            MEASURE = False
 
-sart_time = time.time()
+start_time = time.time()
 
 # Connect to local Pi
 pi = pigpio.pi() 
@@ -22,33 +35,27 @@ pi = pigpio.pi()
 # Check if Pi is Connected
 if not pi.connected:
     print("ERROR: Pi is not connected")    
-    exit(0)
+    sys.exit()
 
-# Defining Pi's input
+# Define Pi's input
 pi.set_mode(GPIO, pigpio.INPUT)
 # Set it to down, i.e., 0 
 pi.set_pull_up_down(GPIO, pigpio.PUD_DOWN)
 
-# Define the callback that will 
+# Define the callback that will execute the sampling
 cb = pi.callback(GPIO, pigpio.RISING_EDGE, array_write)
-
 
 while MEASURE:
     try:
-        if i == (N_TIME_CAPTURES - 1):
-            MEDIR = False
+        pass
     except KeyboardInterrupt:
-        cb.cancel()
-        np.savetxt(capture_array, "logfile_" + str(start_time) + ".csv", delimiter=",")
         break
-        
-        
+                
 cb.cancel() 
 
 end_time = time.time()
 
-np.savetxt(capture_array, "logfile_" + str(start_time) + ".csv", delimiter=",")
-
+np.savetxt(capture_array, "peaksfile_" + str(start_time) + ".csv", delimiter=",")
 
 
 pi.stop()
